f807889425280b94a4c596781fba997a83bbb881
wxb <wxb1@h-sun.com>
2024-10-15 17:30:02 +0800

    设计了登录的大致框架

--- hsunfrontend/src/App.vue
+++ hsunfrontend/src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+    import { RouterLink, RouterView } from 'vue-router'   
+    // import HelloWorld from './components/HelloWorld.vue'
+    import Login from './views/Login.vue';
+
 </script>
 
 <template>
 
+<div>
+    <Login />
+
+</div>
+
 </template>
 
 <style scoped>
-
 </style>
--- hsunfrontend/src/main.ts
+++ hsunfrontend/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+
+
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
@@ -8,13 +10,15 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 import 'element-plus/dist/index.css'
+import 'virtual:windi.css'
+import 'animate.css'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
-
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
